31c141b61ac00938bee1a85a642fe22fda85f17c
Karoly Sefel <karoly@bitventure.co.uk>
2021-10-17 14:16:46 +0100

    Creating the authorization route

--- client.js
+++ client.js
@@ -23,9 +23,9 @@ app.use(timeout)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-/*
-Your code here
-*/
+app.get("/authorize", (req, res) => {
+	res.end();
+})
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
